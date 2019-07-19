f1c85f9462d7be38828a5351c01227efd97d3a89
BrenaElle <brena.ellen@academico.ifpb.edu.br>
2019-07-19 15:00:15 -0300

    Meus Exercícios

---
+++ script6.js
@@ -0,0 +1,30 @@
+
+
+class Pessoa{
+    constructor(){
+        this.nome = ' ',
+        this.sobrenome = ' '
+    }
+
+    nomePessoa(primeironome){
+        this.nome = primeironome;
+        console.log('Meu nome eh:'+ this.nome);
+    }
+    segundoNome(segundoNome){
+        this.sobrenome = segundoNome;
+        console.log('Meu sobrenome eh:'+ this.sobrenome);
+    }
+
+    nomeCompleto(){
+        let nomeCompleto = this.nome + ' ' + this.sobrenome;
+        console.log(nomeCompleto);
+    }
+
+};
+
+let pessoa1 = new Pessoa();
+
+pessoa1.nomePessoa('Matheus');
+pessoa1.segundoNome('Fraga');
+
+pessoa1.nomeCompleto();
